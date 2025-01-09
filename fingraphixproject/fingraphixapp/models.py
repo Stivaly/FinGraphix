23eb5d222dd931f5d01ddfd298ff1b650dc3ae8d
stivaly <stivaly.g@hotmail.com>
2025-01-09 20:35:13 +0000

    Fix project for production

--- fingraphixproject/fingraphixapp/models.py
+++ fingraphixproject/fingraphixapp/models.py
@@ -11,14 +11,14 @@ class Movimiento(models.Model):
     id = models.AutoField(primary_key=True)
     tipo = models.CharField(max_length=7, choices=TIPO_MOVIMIENTO)
     monto = models.DecimalField(max_digits=10, decimal_places=2)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField()
     descripcion = models.TextField()
-    
+
     def clean(self):
         if self.monto is None:
             raise ValidationError("El monto no puede estar vacío.")
         try:
-            int(self.monto)  
+            int(self.monto)
         except ValueError:
             raise ValidationError("El monto debe ser un número entero válido.")
         if int(self.monto) <= 0:
@@ -33,14 +33,12 @@ class Movimiento(models.Model):
     def save(self, *args, **kwargs):
         self.clean()
         super().save(*args, **kwargs)
-    
+
     def monto_formateado(self):
         return "${:,.2f}".format(self.monto)
 
     def __str__(self):
         return f"{self.get_tipo_display()} - {self.monto} - {self.timestamp}"
-    
+
     class Meta:
         db_table = 'movimiento'
-
-
--- fingraphixproject/fingraphixproject/settings.py
+++ fingraphixproject/fingraphixproject/settings.py
@@ -27,12 +27,14 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stivaly2.pythonanywhere.com']
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,11 +122,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = '/home/stivaly2/FinGraphix/staticfiles'
 
 STORAGES = {
     "default": {
